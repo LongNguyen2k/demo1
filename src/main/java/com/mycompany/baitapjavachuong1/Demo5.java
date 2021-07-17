582bd762e90e2507b809f6a5d8e1fad91d80ba5b
LongNguyen2k <instantcy234@gmail.com>
2021-07-17 08:04:28 +0700

    AddBaiTapDemo5

---
+++ src/main/java/com/mycompany/baitapjavachuong1/Demo5.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.baitapjavachuong1;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Demo5 {
+    int a = 1 ;
+    int b = 2 ;
+}
