471fd0ebee534d20748f557449d2260d40bdb08e
LongNguyen2k <instantcy234@gmail.com>
2021-07-17 07:54:39 +0700

    Add Bai Tap 3

--- src/main/java/com/mycompany/baitapjavachuong1/BaiTapDemo.java
+++ src/main/java/com/mycompany/baitapjavachuong1/BaiTapDemo.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.baitapjavachuong1;
-
-import java.util.Random;
-import java.util.Scanner;
-
-/**
- *
- * @author Windows 10
- */
-public class BaiTapDemo {
-       public static void main(String[] args) {
-   
-           System.out.println("Hello BaiTap Demo !");
-    
-       }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.mycompany.baitapjavachuong1;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+///**
+// *
+// * @author Windows 10
+// */
+//public class BaiTapDemo {
+//       public static void main(String[] args) {
+//   
+//           System.out.println("Hello BaiTap Demo !");
+//    
+//       }
+//}
